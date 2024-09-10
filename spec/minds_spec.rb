79677632551a356aa14bfa37104d80c79c269bc3
tungnt1203 <tungnguyen120301@gmail.com>
2024-09-10 13:43:08 +0700

    init commit

---
+++ spec/minds_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Minds do
+  it "has a version number" do
+    expect(Minds::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
