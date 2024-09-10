39af93927215defaa1f9042ed4b7cc2a1ad1f008
tungnt1203 <tungnguyen120301@gmail.com>
2024-09-10 15:57:58 +0700

    connec minds

--- lib/minds.rb
+++ lib/minds.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "minds/version"
+require_relative "minds/config/base"
+require_relative "minds/api/client"
+require_relative "minds/database_config/base"
 
 module Minds
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def config
+      @config ||= Config::Base.new
+    end
+
+    def configure
+      yield(config) if block_given?
+    end
+  end
 end
---
+++ lib/minds/config/base.rb
@@ -0,0 +1,7 @@
+module Minds
+  module Config
+    class Base
+      attr_accessor :api_endpoint, :api_key
+    end
+  end
+end
