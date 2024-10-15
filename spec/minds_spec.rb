2e471d0853140b43fc326ee1495a2b872db61d6c
tungnt1203 <tungnguyen120301@gmail.com>
2024-10-16 00:26:21 +0700

    add test

--- spec/minds_spec.rb
+++ spec/minds_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe Minds do
   end
 
   it "does something useful" do
-    client = Minds::Client.new("test_api_key")
+    client = Minds::Client.new(api_key: "test_api_key")
     expect(client).to be_a(Minds::Client)
   end
 
   describe Minds::Client do
     it "initializes with an API key" do
-      client = Minds::Client.new("test_api_key")
+      client = Minds::Client.new(api_key: "test_api_key")
       expect(client.api_key).to eq("test_api_key")
     end
 
     it "uses default base URL if not provided" do
-      client = Minds::Client.new("test_api_key")
+      client = Minds::Client.new(api_key: "test_api_key")
       expect(client.base_url).to eq(Minds::Config::Base::DEFAULT_BASE_URL)
     end
 
     it "allows setting a custom base URL" do
       custom_url = "https://custom.api.com"
-      client = Minds::Client.new("test_api_key", custom_url)
+      client = Minds::Client.new(api_key: "test_api_key", base_url: custom_url)
       expect(client.base_url).to eq(custom_url)
     end
   end
