cc2ca1135de46856e450a6871abdaeb391f693e7
tungnt1203 <tungnguyen120301@gmail.com>
2024-10-26 23:43:17 +0700

    add vcr test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require "minds"
+require "vcr"
+require "webmock/rspec"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("<API_KEY>") { ENV["MINDS_API_KEY"] || "api_key" }
+  config.filter_sensitive_data("<BASE_URL>") { Minds::Config::Base::DEFAULT_BASE_URL }
+  config.allow_http_connections_when_no_cassette = true
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
